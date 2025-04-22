df9c7bdc9424d8466edc0a9de871cb9b719757c5
Dr. Dee <ready4schooltt@gmail.com>
2025-04-21 20:22:13 -0400

    Fix Header import casing

---
+++ app/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+      <header style={{ 
+        backgroundColor: '#0d47a1', 
+        padding: '1rem', 
+        color: 'white', 
+        textAlign: 'center' 
+      }}>
+        <h1>Ready4SchoolTT</h1>
+      </header>
+    );
+  }
+  
