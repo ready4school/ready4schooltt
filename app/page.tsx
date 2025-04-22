8369fbe458dc4d8fb60383396bb1d3d2d5a0b68e
Dr. Dee <ready4schooltt@gmail.com>
2025-04-21 20:43:45 -0400

    Remove unused Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Header from './components/Header';
 import HeroBanner from './components/HeroBanner'; // ✅ New import
 
 export default function Home() {
   return (
-    <>
-      <Header />
+      <>
       <HeroBanner />
     </>
   );
